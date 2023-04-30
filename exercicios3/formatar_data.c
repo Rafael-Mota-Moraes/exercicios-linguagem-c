d1ba8204510e6fc414ed06a241a9e623e56e990b
Rafael Mota Moraes <rafaelmota622@gmail.com>
2023-04-30 11:47:44 -0300

    Formatar data

---
+++ exercicios3/formatar_data.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+  int dia, mes, ano;
+  int max_dias;
+ 
+  printf("Digite dia:");
+  scanf("%d", &dia);
+  
+  printf("Digite mes:");
+  scanf("%d", &mes);
+  
+  printf("Digite ano:");
+  scanf("%d", &ano);
+
+  if(mes == 2) {
+    max_dias = 28;
+  } else if(mes % 2 == 0) {
+    max_dias = 30;
+  } else {
+    max_dias = 31;
+  }
+
+  if(mes > 12 || mes < 0) {
+    printf("Mês inválido");
+    return 0;
+  }
+
+  if(ano % 4 == 0) {
+    max_dias = 29;
+  }
+
+  if(dia < 0 || dia > max_dias) {
+    printf("Qtd dias inválidos\n");
+    return 0;
+  }
+
+  printf("%d/%d/%d\n", dia, mes, ano);
+
+  return 0;
+}
