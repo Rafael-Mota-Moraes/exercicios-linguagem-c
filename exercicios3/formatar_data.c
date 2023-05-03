f126199b7e82498aaf315a6255ec6d22ad655182
Rafael MMM <75507122+Rafael-Mota-Moraes@users.noreply.github.com>
2023-05-03 20:27:28 -0300

    Update formatar_data.c

--- exercicios3/formatar_data.c
+++ exercicios3/formatar_data.c
@@ -1,41 +1,51 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
   int dia, mes, ano;
   int max_dias;
- 
-  printf("Digite dia:");
+
+  printf("Digite dia: ");
   scanf("%d", &dia);
-  
-  printf("Digite mes:");
+
+  printf("Digite mes: ");
   scanf("%d", &mes);
-  
-  printf("Digite ano:");
+
+  printf("Digite ano: ");
   scanf("%d", &ano);
 
-  if(mes == 2) {
+  if (mes == 2)
+  {
     max_dias = 28;
-  } else if(mes % 2 == 0) {
+  }
+  else if (mes % 2 == 0)
+  {
     max_dias = 30;
-  } else {
+  }
+  else
+  {
     max_dias = 31;
   }
 
-  if(mes > 12 || mes < 0) {
+  if (mes > 12 || mes < 0)
+  {
     printf("Mês inválido");
     return 0;
   }
 
-  if(ano % 4 == 0) {
+  // Checagem do ano bissexto
+  if (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0)
+  {
     max_dias = 29;
   }
 
-  if(dia < 0 || dia > max_dias) {
+  if (dia < 0 || dia > max_dias)
+  {
     printf("Qtd dias inválidos\n");
     return 0;
   }
 
-  printf("%d/%d/%d\n", dia, mes, ano);
+  printf("%02d/%02d/%04d\n", dia, mes, ano);
 
   return 0;
 }
