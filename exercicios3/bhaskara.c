b669c96af873c9136da866987ccd15a0e7ec33a1
Rafael MMM <75507122+Rafael-Mota-Moraes@users.noreply.github.com>
2023-05-03 21:43:30 -0300

    Update bhaskara.c

--- exercicios3/bhaskara.c
+++ exercicios3/bhaskara.c
@@ -3,45 +3,65 @@
 
 // Adicionar flag -lm na hora de compilar
 
-int main() { 
+int main()
+{
   double a, b, c;
- 
+
   printf("Digite A:");
   scanf("%lf", &a);
-  
+
   printf("Digite B:");
   scanf("%lf", &b);
-  
+
   printf("Digite C:");
   scanf("%lf", &c);
 
-  double delta;
-
-  delta = (b * b) - 4 * a * c;
-
-  if(delta < 0) {
-    printf("Delta negativo\n");
-
-    return 0;
+  if (a == 0)
+  {
+    if (a == 0)
+    {
+      printf("Sem resposta, (não é uma equação)");
+    }
+    else
+    {
+      // Equação de 1° grau
+      double x = -c / b;
+      printf("Delta é igual a zero, X = %lf\n", x);
+    }
   }
+  else
+  {
+    // Equação de 2° grau
+    double delta;
+    delta = (b * b) - 4 * a * c;
 
-  double x;
+    if (delta < 0)
+    {
+      printf("Delta negativo, sem resposta\n");
 
-  if(delta == 0) {
-    x = -b / (2 * a);
-    printf("Delta é igual a zero, X = %lf\n", x);
-    return 0;
+      return 0;
+    }
+
+    double x;
+
+    if (delta == 0)
+    {
+      x = (-b) / (2 * a);
+      printf("Delta é igual a zero, X = %lf\n", x);
+      return 0;
+    }
+
+    // Delta é maior que zero
+    double raiz_quadrada_do_delta;
+    double x1, x2;
+
+    raiz_quadrada_do_delta = sqrt(delta);
+
+    x1 = (-b + raiz_quadrada_do_delta) / (2 * a);
+    x2 = (-b - raiz_quadrada_do_delta) / (2 * a);
+
+    printf("X1=%.2lf X2=%.2lf", x1, x2);
   }
-
-  double raiz_quadrada_do_delta;
-  double x1, x2;
-
-  raiz_quadrada_do_delta = sqrt(delta);
-
-  x1 = (-b + raiz_quadrada_do_delta) / (2 * a);
-  x2 = (-b - raiz_quadrada_do_delta) / (2 * a);
-
-  printf("X1=%.2lf X2=%.2lf", x1, x2);
 
   return 0;
 }
