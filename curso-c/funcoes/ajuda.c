75229f3a50a0326366d628353f5031fd8bf765eb
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2023-07-10 12:23:38 -0300

    cabecalhos e bibliotecas próprias

---
+++ curso-c/funcoes/ajuda.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void mensagem() {
+    printf("Olá, essa é minha biblioteca\n");
+}
+
+int soma(int num1, int num2) {
+    return num1 + num2;
+}
+
+int multiplicacao(int num1, int num2) {
+    return num1 * num2;
+}
