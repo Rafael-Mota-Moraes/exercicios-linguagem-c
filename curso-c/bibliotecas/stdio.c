3a9f960470759c8ba4dadec3b0a9a7b02ed5d2df
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2023-07-13 13:03:27 -0300

    stdio.h

---
+++ curso-c/bibliotecas/stdio.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Funções de entrada e saída:
+
+/*
+    Acesso a arquivos.
+
+    fclose();
+    fflush(); limpar buffer. (corrige um bug no windows)
+    fopen();
+
+    Entradas e saídas formatadas
+
+    fscanf();
+
+    printf();
+    scanf();
+
+    Entradas e saídas d caracteres
+
+    gets();
+    fputs();
+    getchar();
+    fgets();
+
+    Tratamento de erros
+
+    feof();
+
+    tipos e macros
+
+    FILE
+    EOF
+    NULL
+
+*/ 
+
+int main() {
+
+
+    return 0;
+}
