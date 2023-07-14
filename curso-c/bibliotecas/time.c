7a7931fe16ece20254b339e7175eefecab1cbafc
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2023-07-14 11:50:12 -0300

    time

---
+++ curso-c/bibliotecas/time.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+
+/*
+FUNÇÕES DE DATA E HORA
+
+clock(); // retorna o número de pulsos de clock desde que o programa foi iniciado
+time(); // retorna o tempo atual do calén dário
+localtime(); // converte um valor para uma hora local
+
+*/
+
+int main() {
+
+    return 0;
+}
