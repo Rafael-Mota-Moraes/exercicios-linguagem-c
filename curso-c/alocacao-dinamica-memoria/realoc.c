940180e2e4e6be3fa8269eb5197a7252d4e80c8d
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2023-07-14 13:20:48 -0300

    Funções para alocação de memória - Realloc

---
+++ curso-c/alocacao-dinamica-memoria/realoc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Realoc - realocação de memória
+
+int main() {
+    int qtd, *p;
+
+    printf("Informe a quantidade de elementos para o vetor: ");
+    scanf("%d", &qtd);
+
+    p = (int*)malloc(qtd * sizeof(int));
+
+    for(int i = 0; i < qtd; i++) {
+        printf("Informe o valor para a posição %d do vetor: ", (i + 1));
+        scanf("%d", &p[i]);
+    }
+
+    for(int i = 0; i < qtd; i++) {
+        printf("No vetor 'numeros[%d]' está o valor: %d\n", i, p[i]);
+    }
+
+    p = (int*)realloc(p, qtd * sizeof(int));
+
+    qtd = qtd * sizeof(int);
+
+    printf("A variável 'p' ocupa %d bytes em memória\n", qtd);
+
+    // liberar a memória
+    free(p);
+    p = NULL; // medida de segurança
+
+    return 0;
+}
