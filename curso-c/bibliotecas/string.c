16c4afb60c01580afb46d5ea473c27f2c5e5fc2d
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2023-07-14 11:43:11 -0300

    string

---
+++ curso-c/bibliotecas/string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+OPERAÇÕES COM STRINGS
+
+strcpy(); // copia de string
+strcat(); // concatena strings
+strcmp(); // compara strings
+strchr(); // localiza a primeira ocorrência de caractere em uma string
+strlen(); // Retorna o tamanho da string
+
+*/
+
+int main() {
+
+    return 0;
+}
